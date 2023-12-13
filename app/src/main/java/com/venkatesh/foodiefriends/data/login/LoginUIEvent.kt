1d4005982aed222afc29b91a33e51faf1336bbb4
W9633770 <w963377@live.tees.ac.uk>
2023-12-13 18:02:39 +0000

    Final Project Changes

--- app/src/main/java/com/venkatesh/foodiefriends/data/login/LoginUIEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.venkatesh.foodiefriends.data.login
-
-sealed class LoginUIEvent{
-
-    data class EmailChanged(val email:String): LoginUIEvent()
-    data class PasswordChanged(val password: String) : LoginUIEvent()
-
-    object LoginButtonClicked : LoginUIEvent()
-}
